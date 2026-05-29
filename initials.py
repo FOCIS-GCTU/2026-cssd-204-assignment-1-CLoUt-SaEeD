05ff68f6efbd1f5dde2523b7dc631a6eb7c1d084
CLoUt-SaEeD <2425402978@live.gctu.edu.gh>
2026-05-29 23:43:12 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,37 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: AFARI MAXWELL JABBAR SAEED
+# STUDENT ID: 2425402978
+# Email: 2425402978@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, AFARI MAXWELL JABBAR SAEED , this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+    # This program prints my initials in stylized large letters
 
+    print()
+    print("...AMS")
+    print()
+
+    print("...AAAAAAAAAAAA........MMMMM....MMMMM........SSSSSSSSSSS......")
+    print("...AAAAAAAAAAAA........MMMMMM..MMMMMM......SSSSSSSSSSSSSS.....")
+    print("...AAAA....AAAA........MMMMMMM.MMMMMM.....SSSS........SSSS....")
+    print("...AAAA....AAAA........MMMMMMMMMMMMMM.....SSSS...............")
+    print("...AAAAAAAAAAAA........MMMM.MMMMM.MMMM......SSSSSSSSSSS.......")
+    print("...AAAAAAAAAAAA........MMMM..MMM..MMMM.......SSSSSSSSSSS......")
+    print("...AAAA....AAAA........MMMM.......MMMM...............SSSS.....")
+    print("...AAAA....AAAA........MMMM.......MMMM.....SSSS........SSSS...")
+    print("...AAAA....AAAA........MMMM.......MMMM......SSSSSSSSSSSSSS.....")
+    print("...AAAA....AAAA........MMMM.......MMMM.......SSSSSSSSSSS.......")
+
+    print()
 
 
 main()
+
